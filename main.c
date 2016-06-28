f66433c972356c5fe9309aabb5028ec275dafc20
newenclave <pikrat.acid@gmail.com>
2016-06-28 15:08:33 +0300

    add array

--- main.c
+++ main.c
@@ -1,4 +1,23 @@
 #include <stdio.h>
+#include "arrays.h"
+
+
+#define BACKETS_COUNT 17
+
+typedef struct  _value {
+
+} value;
+
+typedef struct _key_value_pair {
+    size_t key_;
+    value  val_;
+} key_value_pair;
+
+array_define_custom_type( key_value_pair, queue );
+
+typedef struct _backet_node {
+
+} backet_node;
 
 int main( )
 {
